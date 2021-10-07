dfe4ad0fa4c05d29e146df7bf946f76032144f7c
herbberg <herbert.bergauer@oeaw.ac.at>
2021-10-07 13:28:45 +0200

    updated

--- tmVhdlProducer/templates/vhdl/gtl_luts.vhd
+++ tmVhdlProducer/templates/vhdl/gtl_luts.vhd
@@ -13,103 +13,116 @@
 {%- endmacro -%}
 
 -- eg and tau pt LUTs
-type eg_pt_lut_array is array (0 to 2**(EG_ET_HIGH-EG_ET_LOW+1)-1) of natural range {{eg_pt_min}} to {{eg_pt_max}};
+type eg_pt_lut_array is array (0 to 2**(EG_ET_HIGH-EG_ET_LOW+1)-1) of natural range {{eg_param['min']}} to {{eg_param['max']}};
 
 constant EG_PT_LUT : eg_pt_lut_array := (
-{{ dump_lut(eg_pt_ll, v_p_r, eg_pt_lut)|trim }}
+{{ dump_lut(eg_param['ll'], v_p_r, eg_param['lut'])|trim }}
 );
 
 -- jet pt LUT
-type jet_pt_lut_array is array (0 to 2**(JET_ET_HIGH-JET_ET_LOW+1)-1) of natural range {{jet_pt_min}} to {{jet_pt_max}};
+type jet_pt_lut_array is array (0 to 2**(JET_ET_HIGH-JET_ET_LOW+1)-1) of natural range {{jet_param['min']}} to {{jet_param['max']}};
 
 constant JET_PT_LUT : jet_pt_lut_array := (
-{{ dump_lut(jet_pt_ll, v_p_r, jet_pt_lut)|trim }}
+{{ dump_lut(jet_param['ll'], v_p_r, jet_param['lut'])|trim }}
 );
 
 -- esums pt LUTs
-type etm_pt_lut_array is array (0 to 2**(ETM_ET_HIGH-ETM_ET_LOW+1)-1) of natural range {{etm_pt_min}} to {{etm_pt_max}};
+type etm_pt_lut_array is array (0 to 2**(ETM_ET_HIGH-ETM_ET_LOW+1)-1) of natural range {{etm_param['min']}} to {{etm_param['max']}};
 
 constant ETM_PT_LUT : etm_pt_lut_array := (
-{{ dump_lut(etm_pt_ll, v_p_r, etm_pt_lut)|trim }}
+{{ dump_lut(etm_param['ll'], v_p_r, etm_param['lut'])|trim }}
 );
 
 -- muon pt LUT
-type muon_pt_lut_array is array (0 to 2**(MUON_PT_HIGH-MUON_PT_LOW+1)-1) of natural range {{mu_pt_min}} to {{mu_pt_max}};
+type muon_pt_lut_array is array (0 to 2**(MUON_PT_HIGH-MUON_PT_LOW+1)-1) of natural range {{mu_pt_param['min']}} to {{mu_pt_param['max']}};
 
 constant MU_PT_LUT : muon_pt_lut_array := (
-{{ dump_lut(mu_pt_ll, v_p_r, mu_pt_lut)|trim }}
+{{ dump_lut(mu_pt_param['ll'], v_p_r, mu_pt_param['lut'])|trim }}
 );
 
 -- muon unconstraint pt LUT
-type muon_upt_lut_array is array (0 to 2**(MUON_UPT_HIGH-MUON_UPT_LOW+1)-1) of natural range {{mu_upt_min}} to {{mu_upt_max}};
+type muon_upt_lut_array is array (0 to 2**(MUON_UPT_HIGH-MUON_UPT_LOW+1)-1) of natural range {{mu_upt_param['min']}} to {{mu_upt_param['max']}};
 
 constant MU_UPT_LUT : muon_upt_lut_array := (
-{{ dump_lut(mu_upt_ll, v_p_r, mu_upt_lut)|trim }}
+{{ dump_lut(mu_upt_param['ll'], v_p_r, mu_upt_param['lut'])|trim }}
 );
 
 -- calo-calo differences LUTs
-type calo_calo_diff_eta_lut_array is array (0 to 2**MAX_CALO_ETA_BITS-1) of natural range {{cc_deta_min}} to {{cc_deta_max}};
+type calo_calo_diff_eta_lut_array is array (0 to 2**MAX_CALO_ETA_BITS-1) of natural range {{cc_deta_param['min']}} to {{cc_deta_param['max']}};
 
 constant CALO_CALO_DIFF_ETA_LUT : calo_calo_diff_eta_lut_array := (
-{{ dump_lut(cc_deta_ll, v_p_r, cc_deta_lut)|trim }}
+{{ dump_lut(cc_deta_param['ll'], v_p_r, cc_deta_param['lut'])|trim }}
 );
 
-type calo_calo_diff_phi_lut_array is array (0 to 2**MAX_CALO_PHI_BITS-1) of natural range {{cc_dphi_min}} to {{cc_dphi_max}};
+type calo_calo_diff_phi_lut_array is array (0 to 2**MAX_CALO_PHI_BITS-1) of natural range {{cc_dphi_param['min']}} to {{cc_dphi_param['max']}};
 
 constant CALO_CALO_DIFF_PHI_LUT : calo_calo_diff_phi_lut_array := (
-{{ dump_lut(cc_dphi_ll, v_p_r, cc_dphi_lut)|trim }}
+{{ dump_lut(cc_dphi_param['ll'], v_p_r, cc_dphi_param['lut'])|trim }}
 );
 
 -- muon-muon differences LUTs
-type muon_muon_diff_eta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1)-1) of natural range {{mm_deta_min}} to {{mm_deta_max}};
+type muon_muon_diff_eta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1)-1) of natural range {{mm_deta_param['min']}} to {{mm_deta_param['max']}};
 
 constant MU_MU_DIFF_ETA_LUT : muon_muon_diff_eta_lut_array := (
-{{ dump_lut(mm_deta_ll, v_p_r, mm_deta_lut)|trim }}
+{{ dump_lut(mm_deta_param['ll'], v_p_r, mm_deta_param['lut'])|trim }}
 );
 
-type muon_muon_diff_phi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of natural range {{mm_dphi_min}} to {{mm_dphi_max}};
+type muon_muon_diff_phi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of natural range {{mm_dphi_param['min']}} to {{mm_dphi_param['max']}};
 
 constant MU_MU_DIFF_PHI_LUT : muon_muon_diff_phi_lut_array := (
-{{ dump_lut(mm_dphi_ll, v_p_r, mm_dphi_lut)|trim }}
+{{ dump_lut(mm_dphi_param['ll'], v_p_r, mm_dphi_param['lut'])|trim }}
 );
 
 -- calo-muon differences LUTs
-type calo_muon_diff_eta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1+1)-1) of natural range {{cm_deta_min}} to {{cm_deta_max}};
+type calo_muon_diff_eta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1+1)-1) of natural range {{cm_deta_param['min']}} to {{cm_deta_param['max']}};
 
 constant CALO_MU_DIFF_ETA_LUT : calo_muon_diff_eta_lut_array := (
-{{ dump_lut(cm_deta_ll, v_p_r, cm_deta_lut)|trim }}
+{{ dump_lut(cm_deta_param['ll'], v_p_r, cm_deta_param['lut'])|trim }}
 );
 
-type calo_muon_diff_phi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of natural range {{cm_dphi_min}} to {{cm_dphi_max}};
+type calo_muon_diff_phi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of natural range {{cm_dphi_param['min']}} to {{cm_dphi_param['max']}};
 
 constant CALO_MU_DIFF_PHI_LUT : calo_muon_diff_phi_lut_array := (
-{{ dump_lut(cm_dphi_ll, v_p_r, cm_dphi_lut)|trim }}
+{{ dump_lut(cm_dphi_param['ll'], v_p_r, cm_dphi_param['lut'])|trim }}
 );
 
 -- calo-calo cosh deta LUTs
-type calo_calo_cosh_deta_lut_array is array (0 to 2**MAX_CALO_ETA_BITS-1) of natural range {{cc_cosh_deta_min}} to {{cc_cosh_deta_max}};
+type calo_calo_cosh_deta_lut_array is array (0 to 2**MAX_CALO_ETA_BITS-1) of natural range {{cc_cosh_deta_param['min']}} to {{cc_cosh_deta_param['max']}};
 
 constant CALO_CALO_COSH_DETA_LUT : calo_calo_cosh_deta_lut_array := (
-{{ dump_lut(cc_cosh_deta_ll, v_p_r, cc_cosh_deta_lut)|trim }}
+{{ dump_lut(cc_cosh_deta_param['ll'], v_p_r, cc_cosh_deta_param['lut'])|trim }}
 );
 
 -- calo-calo cos dphi LUTs
-type calo_calo_cos_dphi_lut_array is array (0 to 2**MAX_CALO_PHI_BITS-1) of integer range {{cc_cos_dphi_min}} to {{cc_cos_dphi_max}};
+type calo_calo_cos_dphi_lut_array is array (0 to 2**MAX_CALO_PHI_BITS-1) of integer range {{cc_cos_dphi_param['min']}} to {{cc_cos_dphi_param['max']}};
 
 constant CALO_CALO_COS_DPHI_LUT : calo_calo_cos_dphi_lut_array := (
-{{ dump_lut(cc_cos_dphi_ll, v_p_r, cc_cos_dphi_lut)|trim }}
+{{ dump_lut(cc_cos_dphi_param['ll'], v_p_r, cc_cos_dphi_param['lut'])|trim }}
 );
 
 -- muon-muon cosh deta LUTs
-type muon_muon_cosh_deta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1)-1) of natural range {{mm_cosh_deta_min}} to {{mm_cosh_deta_max}};
+type calo_muon_cosh_deta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1+1)-1) of natural range {{cm_cosh_deta_param['min']}} to {{cm_cosh_deta_param['max']}};
+
+constant CALO_MUON_COSH_DETA_LUT : calo_muon_cosh_deta_lut_array := (
+{{ dump_lut(cm_cosh_deta_param['ll'], v_p_r, cm_cosh_deta_param['lut'])|trim }}
+);
+
+-- calo-muon cos dphi LUTs
+type calo_muon_cos_dphi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of integer range {{cm_cos_dphi_param['min']}} to {{cm_cos_dphi_param['max']}};
+
+constant CALO_MUON_COS_DPHI_LUT : calo_muon_cos_dphi_lut_array := (
+{{ dump_lut(cm_cos_dphi_param['ll'], v_p_r, cm_cos_dphi_param['lut'])|trim }});
+
+-- muon-muon cosh deta LUTs
+type muon_muon_cosh_deta_lut_array is array (0 to 2**(MUON_ETA_HIGH-MUON_ETA_LOW+1)-1) of natural range {{mm_cosh_deta_param['min']}} to {{mm_cosh_deta_param['max']}};
 
 constant MU_MU_COSH_DETA_LUT : muon_muon_cosh_deta_lut_array := (
-{{ dump_lut(mm_cosh_deta_ll, v_p_r, mm_cosh_deta_lut)|trim }}
+{{ dump_lut(mm_cosh_deta_param['ll'], v_p_r, mm_cosh_deta_param['lut'])|trim }}
 );
 
 -- muon-muon cos dphi LUTs
-type muon_muon_cos_dphi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of integer range {{mm_cos_dphi_min}} to {{mm_cos_dphi_max}};
+type muon_muon_cos_dphi_lut_array is array (0 to 2**(MUON_PHI_HIGH-MUON_PHI_LOW+1)-1) of integer range {{mm_cos_dphi_param['min']}} to {{mm_cos_dphi_param['max']}};
 
 constant MU_MU_COS_DPHI_LUT : muon_muon_cos_dphi_lut_array := (
-{{ dump_lut(mm_cos_dphi_ll, v_p_r, mm_cos_dphi_lut)|trim }}
+{{ dump_lut(mm_cos_dphi_param['ll'], v_p_r, mm_cos_dphi_param['lut'])|trim }}
 );
--- tmVhdlProducer/vhdlproducer.py
+++ tmVhdlProducer/vhdlproducer.py
@@ -101,14 +101,12 @@ def PtLutsCalc(bits, bins, step, prec, pt_bin_min, pt_bin_max):
     lut_val = [0 for x in range(lut_len)]
 
     # pt luts
-    #if lut_type == "pt":
     for i in range(0,lut_len):
         lut[i] = int(round_halfway(((pt_bin_max[i] - pt_bin_min[i])/2+pt_bin_min[i])*10**prec))
         if i < bins:
             lut_val[i] = lut[i]
         else:
             lut_val[i] = 0
-
     return lut_val
 
 def DeltaLutsCalc(lut_type, bits, bins, step, prec):
@@ -131,8 +129,6 @@ def DeltaLutsCalc(lut_type, bits, bins, step, prec):
                 else:
                     lut_val[i] = 0
 
-    return lut_val
-
     # TODO
     # sin phi and cos phi luts
     #if lut_type == "sin_cos_phi":
@@ -143,42 +139,29 @@ def DeltaLutsCalc(lut_type, bits, bins, step, prec):
     return lut_val
 
 def GtlLutsGenerator(self, scales, directory):
-    # calculate LUT values for deta and dphi
-    for corr_type in ["calo_calo", "calo_muon", "muon_muon"]:
-        if corr_type == "calo_calo":
-            eta_type = 'EG-ETA'
-            phi_type = 'EG-PHI'
-            delta_prec = scales['PRECISION-EG-EG-Delta'].getNbits()
-            math_prec = scales['PRECISION-EG-EG-Math'].getNbits()
-        elif corr_type == "calo_muon":
-            phi_type = 'MU-PHI'
-            delta_prec = scales['PRECISION-EG-MU-Delta'].getNbits()
-            math_prec = scales['PRECISION-EG-MU-Math'].getNbits()
-        elif corr_type == "muon_muon":
-            eta_type = 'MU-ETA'
-            phi_type = 'MU-PHI'
-            delta_prec = scales['PRECISION-MU-MU-Delta'].getNbits()
-            math_prec = scales['PRECISION-MU-MU-Math'].getNbits()
+    #for corr_type in ["EG-EG", "EG-MU", "MU-MU"]:
+    for corr_type in ["EG-EG", "EG-MU", "MU-MU"]:
+        eta_type = corr_type.split('-')[0]+"-ETA"
+        phi_type = corr_type.split('-')[1]+"-PHI"
 
-        if corr_type == "calo_muon":
-            eta_max_value = scales['EG-ETA'].getMaximum()
-            eta_min_value = scales['EG-ETA'].getMinimum()
-            eta_step = scales['MU-ETA'].getStep()
-            eta_bins = int((abs(eta_min_value)+eta_max_value)/eta_step)+1
-            # find next eta_bits (next greater 2**x value of eta_bins)
-            for i in range(0,20):
-                if eta_bins >= 2**i and eta_bins < 2**(i+1):
-                    eta_bits = i+1
-        else:
-            eta_bits = scales[eta_type].getNbits()
-            eta_max_value = scales[eta_type].getMaximum()
-            eta_min_value = scales[eta_type].getMinimum()
-            eta_step = scales[eta_type].getStep()
-            eta_bins = int((abs(scales[eta_type].getMinimum())+scales[eta_type].getMaximum())/scales[eta_type].getStep())+1
+        eta_bits = scales[eta_type].getNbits()
+        eta_max_value = scales[eta_type].getMaximum()
+        eta_min_value = scales[eta_type].getMinimum()
+        eta_step = scales[eta_type].getStep()
 
         phi_bits = scales[phi_type].getNbits()
         phi_step = scales[phi_type].getStep()
         phi_bins = int(scales[phi_type].getMaximum()/scales[phi_type].getStep())
+
+        delta_prec = scales['PRECISION-'+corr_type+'-Delta'].getNbits()
+        math_prec = scales['PRECISION-'+corr_type+'-Math'].getNbits()
+
+        if corr_type == "EG-MU":
+            eta_bits = scales['MU-ETA'].getNbits()+1 # +1 for correct len of calo-muon cosh deta lut
+            eta_step = scales['MU-ETA'].getStep()
+
+        eta_bins = int(round_halfway((abs(eta_min_value)+eta_max_value)/eta_step))
+        print("eta_bits:", eta_bits, ", eta_step:", eta_step, ", eta_bins:", eta_bins, ", eta_max_value:", eta_max_value, ", eta_min_value:", eta_min_value)
 
         for lut_type in ["deta", "dphi", "cosh_deta", "cos_dphi"]:
             if lut_type == "deta":
@@ -193,81 +176,53 @@ def GtlLutsGenerator(self, scales, directory):
             max_val = max(lut_val)
             min_val = min(lut_val)
 
-            if corr_type == "calo_calo" and lut_type == "deta":
-                cc_deta_ll = 2**eta_bits
-                cc_deta_lut_val = lut_val
-                cc_deta_max = max_val
-                cc_deta_min = min_val
-            elif corr_type == "calo_calo" and lut_type == "cosh_deta":
-                cc_cosh_deta_ll = 2**eta_bits
-                cc_cosh_deta_lut_val = lut_val
-                cc_cosh_deta_max = max_val
-                cc_cosh_deta_min = min_val
-            elif corr_type == "calo_calo" and lut_type == "dphi":
-                cc_dphi_ll = 2**phi_bits
-                cc_dphi_lut_val = lut_val
-                cc_dphi_max = max_val
-                cc_dphi_min = min_val
-            elif corr_type == "calo_calo" and lut_type == "cos_dphi":
-                cc_cos_dphi_ll = 2**phi_bits
-                cc_cos_dphi_lut_val = lut_val
-                cc_cos_dphi_max = max_val
-                cc_cos_dphi_min = min_val
-            elif corr_type == "calo_muon" and lut_type == "deta":
-                cm_deta_ll = 2**eta_bits
-                cm_deta_lut_val = lut_val
-                cm_deta_max = max_val
-                cm_deta_min = min_val
-            elif corr_type == "calo_muon" and lut_type == "dphi":
-                cm_dphi_ll = 2**phi_bits
-                cm_dphi_lut_val = lut_val
-                cm_dphi_max = max_val
-                cm_dphi_min = min_val
-            elif corr_type == "muon_muon" and lut_type == "deta":
-                mm_deta_ll = 2**eta_bits
-                mm_deta_lut_val = lut_val
-                mm_deta_max = max_val
-                mm_deta_min = min_val
-            elif corr_type == "muon_muon" and lut_type == "cosh_deta":
-                mm_cosh_deta_ll = 2**eta_bits
-                mm_cosh_deta_lut_val = lut_val
-                mm_cosh_deta_max = max_val
-                mm_cosh_deta_min = min_val
-            elif corr_type == "muon_muon" and lut_type == "dphi":
-                mm_dphi_ll = 2**phi_bits
-                mm_dphi_lut_val = lut_val
-                mm_dphi_max = max_val
-                mm_dphi_min = min_val
-            elif corr_type == "muon_muon" and lut_type == "cos_dphi":
-                mm_cos_dphi_ll = 2**phi_bits
-                mm_cos_dphi_lut_val = lut_val
-                mm_cos_dphi_max = max_val
-                mm_cos_dphi_min = min_val
+            if lut_type == "deta" or lut_type == "cosh_deta":
+                param = {'ll': 2**eta_bits, 'min': min_val, 'max':max_val, 'lut': lut_val}
+            elif lut_type == "dphi" or lut_type == "cos_dphi":
+                param = {'ll': 2**phi_bits, 'min': min_val, 'max':max_val, 'lut': lut_val}
+
+            if corr_type == "EG-EG":
+                if lut_type == "deta":
+                    cc_deta_param = param
+                elif lut_type == "cosh_deta":
+                    cc_cosh_deta_param = param
+                elif lut_type == "dphi":
+                    cc_dphi_param = param
+                elif lut_type == "cos_dphi":
+                    cc_cos_dphi_param = param
+            elif corr_type == "EG-MU":
+                if lut_type == "deta":
+                    cm_deta_param = param
+                elif lut_type == "cosh_deta":
+                    cm_cosh_deta_param = param
+                elif lut_type == "dphi":
+                    cm_dphi_param = param
+                elif lut_type == "cos_dphi":
+                    cm_cos_dphi_param = param
+            elif corr_type == "MU-MU":
+                if lut_type == "deta":
+                    mm_deta_param = param
+                elif lut_type == "cosh_deta":
+                    mm_cosh_deta_param = param
+                elif lut_type == "dphi":
+                    mm_dphi_param = param
+                elif lut_type == "cos_dphi":
+                    mm_cos_dphi_param = param
 
     # calculate LUT values for deta and dphi
-    for obj_type in ["eg", "jet", "etm", "mu", "mu_upt"]:
-        if obj_type == "eg":
-            pt_type = 'EG-ET'
-        elif obj_type == "jet":
-            pt_type = 'JET-ET'
-        elif obj_type == "etm":
-            pt_type = 'ETM-ET'
-        elif obj_type == "mu":
-            pt_type = 'MU-ET'
-        elif obj_type == "mu_upt":
-            pt_type = 'MU-UPT'
+    pt_scales = ['EG-ET', 'JET-ET', 'ETM-ET', 'MU-ET', 'MU-UPT']
+    for obj_idx, pt_scale in enumerate(pt_scales):
 
-        pt_bits = scales[pt_type].getNbits()
-        pt_max_value = scales[pt_type].getMaximum()
-        pt_step = scales[pt_type].getStep()
+        pt_bits = scales[pt_scale].getNbits()
+        pt_max_value = scales[pt_scale].getMaximum()
+        pt_step = scales[pt_scale].getStep()
         pt_prec = 1 # no definition in scales!
 
-        list_len = int(pt_max_value/pt_step)
-        #print("list_len", list_len)
-        pt_bin_min = [0 for x in range(list_len+1)]
-        pt_bin_max = [0 for x in range(list_len+1)]
-        #idx=0
-        for pt_bin in scales[pt_type].getBins():
+        scale_len = int(pt_max_value/pt_step)
+        pt_bin_min = [0 for x in range(scale_len+1)]
+        pt_bin_max = [0 for x in range(scale_len+1)]
+
+        for pt_bin in scales[pt_scale].getBins():
             pt_bin_min[pt_bin.hw_index] = pt_bin.minimum
             pt_bin_max[pt_bin.hw_index] = pt_bin.maximum
         nr_bins = pt_bin.hw_index+1
@@ -275,32 +230,19 @@ def GtlLutsGenerator(self, scales, directory):
         lut_val = PtLutsCalc(pt_bits, nr_bins, pt_step, pt_prec, pt_bin_min, pt_bin_max)
         max_val = max(lut_val)
         min_val = min(lut_val)
+        ll = 2**pt_bits
+        param_dict = {'ll': ll, 'min': min_val, 'max': max_val, 'lut': lut_val}
 
-        if obj_type == "eg":
-            eg_pt_ll = 2**pt_bits
-            eg_pt_lut_val = lut_val
-            eg_pt_max = max_val
-            eg_pt_min = min_val
-        elif obj_type == "jet":
-            jet_pt_ll = 2**pt_bits
-            jet_pt_lut_val = lut_val
-            jet_pt_max = max_val
-            jet_pt_min = min_val
-        elif obj_type == "etm":
-            etm_pt_ll = 2**pt_bits
-            etm_pt_lut_val = lut_val
-            etm_pt_max = max_val
-            etm_pt_min = min_val
-        elif obj_type == "mu":
-            mu_pt_ll = 2**pt_bits
-            mu_pt_lut_val = lut_val
-            mu_pt_max = max_val
-            mu_pt_min = min_val
-        elif obj_type == "mu_upt":
-            mu_upt_ll = 2**pt_bits
-            mu_upt_lut_val = lut_val
-            mu_upt_max = max_val
-            mu_upt_min = min_val
+        if pt_scale == 'EG-ET':
+            eg_param = param_dict
+        elif pt_scale == 'JET-ET':
+            jet_param = param_dict
+        elif pt_scale == 'ETM-ET':
+            etm_param = param_dict
+        elif pt_scale == 'MU-ET':
+            mu_pt_param = param_dict
+        elif pt_scale == 'MU-UPT':
+            mu_upt_param = param_dict
 
 # render template
     lut_dir = "vhdl_gtl_luts"
@@ -314,23 +256,23 @@ def GtlLutsGenerator(self, scales, directory):
 
     gtl_luts_params = {
         'v_p_r': v_p_r,
-        'eg_pt_ll': eg_pt_ll, 'eg_pt_lut': eg_pt_lut_val, 'eg_pt_max': eg_pt_max, 'eg_pt_min': eg_pt_min,
-        'jet_pt_ll': jet_pt_ll, 'jet_pt_lut': jet_pt_lut_val, 'jet_pt_max': jet_pt_max, 'jet_pt_min': jet_pt_min,
-        'etm_pt_ll': etm_pt_ll, 'etm_pt_lut': etm_pt_lut_val, 'etm_pt_max': etm_pt_max, 'etm_pt_min': etm_pt_min,
-        'mu_pt_ll': mu_pt_ll, 'mu_pt_lut': mu_pt_lut_val, 'mu_pt_max': mu_pt_max, 'mu_pt_min': mu_pt_min,
-        'mu_upt_ll': mu_upt_ll, 'mu_upt_lut': mu_upt_lut_val, 'mu_upt_max': mu_upt_max, 'mu_upt_min': mu_upt_min,
-        'cc_deta_ll': cc_deta_ll, 'cc_deta_min': cc_deta_min, 'cc_deta_max': cc_deta_max, 'cc_deta_lut': cc_deta_lut_val,
-        'cc_cosh_deta_ll': cc_cosh_deta_ll, 'cc_cosh_deta_min': cc_cosh_deta_min, 'cc_cosh_deta_max': cc_cosh_deta_max, 'cc_cosh_deta_lut': cc_cosh_deta_lut_val,
-        'cc_dphi_ll': cc_dphi_ll, 'cc_dphi_min': cc_dphi_min, 'cc_dphi_max': cc_dphi_max, 'cc_dphi_lut': cc_dphi_lut_val,
-        'cc_cos_dphi_ll': cc_cos_dphi_ll, 'cc_cos_dphi_min': cc_cos_dphi_min, 'cc_cos_dphi_max': cc_cos_dphi_max, 'cc_cos_dphi_lut': cc_cos_dphi_lut_val,
-        'cm_deta_ll': cm_deta_ll, 'cm_deta_min': cm_deta_min, 'cm_deta_max': cm_deta_max, 'cm_deta_lut': cm_deta_lut_val,
-        #'cm_cosh_deta_ll': cm_cosh_deta_ll, 'cm_cosh_deta_min': cm_cosh_deta_min, 'cm_cosh_deta_max': cm_cosh_deta_max, 'cm_cosh_deta_lut': cm_cosh_deta_lut_val,
-        'cm_dphi_ll': cm_dphi_ll, 'cm_dphi_min': cm_dphi_min, 'cm_dphi_max': cm_dphi_max, 'cm_dphi_lut': cm_dphi_lut_val,
-        #'cm_cos_dphi_ll': cm_cos_dphi_ll, 'cm_cos_dphi_min': cm_cos_dphi_min, 'cm_cos_dphi_max': cm_cos_dphi_max, 'cm_cos_dphi_lut': cm_cos_dphi_lut_val,
-        'mm_deta_ll': mm_deta_ll, 'mm_deta_min': mm_deta_min, 'mm_deta_max': mm_deta_max, 'mm_deta_lut': mm_deta_lut_val,
-        'mm_cosh_deta_ll': mm_cosh_deta_ll, 'mm_cosh_deta_min': mm_cosh_deta_min, 'mm_cosh_deta_max': mm_cosh_deta_max, 'mm_cosh_deta_lut': mm_cosh_deta_lut_val,
-        'mm_dphi_ll': mm_dphi_ll, 'mm_dphi_min': mm_dphi_min, 'mm_dphi_max': mm_dphi_max, 'mm_dphi_lut': mm_dphi_lut_val,
-        'mm_cos_dphi_ll': mm_cos_dphi_ll, 'mm_cos_dphi_min': mm_cos_dphi_min, 'mm_cos_dphi_max': mm_cos_dphi_max, 'mm_cos_dphi_lut': mm_cos_dphi_lut_val,
+        'eg_param': eg_param,
+        'jet_param': jet_param,
+        'etm_param': etm_param,
+        'mu_pt_param': mu_pt_param,
+        'mu_upt_param': mu_upt_param,
+        'cc_deta_param': cc_deta_param,
+        'cc_cosh_deta_param': cc_cosh_deta_param,
+        'cc_dphi_param': cc_dphi_param,
+        'cc_cos_dphi_param': cc_cos_dphi_param,
+        'cm_deta_param': cm_deta_param,
+        'cm_cosh_deta_param': cm_cosh_deta_param,
+        'cm_dphi_param': cm_dphi_param,
+        'cm_cos_dphi_param': cm_cos_dphi_param,
+        'mm_deta_param': mm_deta_param,
+        'mm_cosh_deta_param': mm_cosh_deta_param,
+        'mm_dphi_param': mm_dphi_param,
+        'mm_cos_dphi_param': mm_cos_dphi_param,
     }
 
     content_luts = self.engine.render(templ_luts, gtl_luts_params)
